5dda75521bf598c187c4a1c7e44a395149fdc1de
nsornman <nachakhun1998@gmail.com>
2025-04-21 22:55:33 +0700

    Change file location and add php file

---
+++ PHP/connect.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Database username
+$password = ""; // Database password        
+$dbname = "my_database"; // Database name
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
