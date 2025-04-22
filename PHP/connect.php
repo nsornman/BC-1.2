eadceb361e6e46325864afb4b5e2ebc0db421dac
nsornman <nachakhun1998@gmail.com>
2025-04-22 07:38:52 +0700

    before disaster

--- PHP/connect.php
+++ PHP/connect.php
@@ -1,8 +1,17 @@
 <?php
+
 $servername = "localhost";
 $username = "root"; // Database username
 $password = ""; // Database password        
-$dbname = "my_database"; // Database name
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$dbname = "example"; // Database name
+$port= NULL; // Database port, NULL for default
+$soket = NULL; // Database socket, NULL for default
+// Create connection    
+$connect = mysqli_connect($servername, $username, $password, $dbname, $port, $soket);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error($connect));
+} else {
+    mysqli_set_charset($connect, "utf8"); // Set character set to utf8mb4
+}
 ?>
