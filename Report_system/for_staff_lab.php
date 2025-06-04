fa1edc06fdcab3a3e96c926f7d38c36f08f8b3f5
nsornman <nachakhun1998@gmail.com>
2025-06-04 19:16:12 +0700

    New report web ui

--- Report_system/for_staff_lab.php
+++ Report_system/for_staff_lab.php
@@ -13,6 +13,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
 </head>
+
 <body>
     <nav class="navbar">
         <div class="logo-info">
@@ -41,7 +42,7 @@
         </thead>
         <tbody>
             <?php 
-                $query = "SELECT case_id, problem_type, place, report_date, status FROM report ORDER BY report_date DESC";
+                $query = "SELECT case_id, problem_type, place, report_date, description, img, status FROM report ORDER BY report_date DESC";
                 $result = mysqli_query($connect, $query);
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) {
@@ -51,20 +52,25 @@
                         echo "<td>" . substr($row['report_date'], 0, 10) . "</td>";
                         echo "<td class = 'status'>" . htmlspecialchars($row['status']) . '
                             <div class="case-info">
-                                <button popovertarget = "' . $infoId . '" class = icon-button> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="width: 1.5rem; height: 1.5rem; vertical-align: middle; margin-left: 5px;">
+                                <button popovertarget = "' . $infoId . '" class = "icon-button" id = "icon-button"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="width: 1.5rem; height: 1.5rem; vertical-align: middle; margin-left: 5px;">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
                                     </svg>
-                                </button> 
-                                <div id="' . $infoId . '" class="case-detail" popover>                                                    
+                                </button>  
+                                <div id="' . $infoId . '" class="case-detail"  popover>                                                    
                                     <div class = "header">
-                                        <div class = "place">' . htmlspecialchars($row['place']) . '</div>
-                                        <div class = "report_date">' . substr($row['report_date'], 0, 10) . '</div>
+                                        <div class = "case-title">
+                                            <div class = "place">' . htmlspecialchars($row['place']) . '</div>
+                                            <div class = "report_date">' . substr($row['report_date'], 0, 10) . '</div>
+                                        </div>                 
                                         <div>' . htmlspecialchars($row['status']) . '</div>
                                     </div>
                                     <div class = "line"></div>
-                                    <p class = "case-id">  รหัสเคส :  '. htmlspecialchars($row['case_id']) . '</p>
-                                        
-                                    
+                                    <div class = "case-detail-content">
+                                        <div class = "case-id">  รหัสเคส :  '. htmlspecialchars($row['case_id']) . '</div>
+                                        <div class = "problem-type">ประเภทปัญหา : ' . htmlspecialchars($row['problem_type']) . '</div>
+                                        <div class = "description">รายละเอียดปัญหา : '. htmlspecialchars($row['description']) . '</div>
+                                        <div class = "img"><img src="' . htmlspecialchars($row['img']) . '" alt="Image" style="max-width: 300px;"></div>
+                                    </div>  
                                 </div>   
                             </div>
                         </td>';
@@ -90,7 +96,7 @@
                         }
                     });
                 });
-            </script>
+            </script>  
         </tbody>
     </table>
 </body>
